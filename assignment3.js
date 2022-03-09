fc1d44389464f9b4126d4832c5796a7920d1fc1e
justinHe123 <justinhe@ucla.edu>
2022-03-09 10:06:03 -0800

    change scene name

--- assignment3.js
+++ assignment3.js
@@ -6,7 +6,7 @@ const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene, Texture,
 } = tiny;
 
-export class Assignment3 extends Scene {
+export class DontFall extends Scene {
     constructor() {
         // constructor(): Scenes begin by populating initial values like the Shapes and Materials they'll need.
         super();
